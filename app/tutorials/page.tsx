8696ed409c92b28aec7d2ca4038014754a3e1beb
faridir55 <faridir55@users.noreply.github.com>
2026-05-11 19:21:40 +0300

    v4.1.7

--- app/tutorials/page.tsx
+++ app/tutorials/page.tsx
@@ -22,9 +22,9 @@ export default function Tutorials() {
                 1. How to Register an Account
               </h2>
               <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg border border-accent-color/20 shadow-[0_4px_15px_rgba(0,0,0,0.5)]">
-                <iframe 
+                <iframe
                   className="absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/UE7r7u8Iqsc?si=EP3QVUO517dVe-Tt" 
+                  src="https://www.youtube.com/embed/UE7r7u8Iqsc?si=EP3QVUO517dVe-Tt"
                   title="How to Register an Account"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -38,9 +38,24 @@ export default function Tutorials() {
                 2. How to Verify Your Account
               </h2>
               <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg border border-accent-color/20 shadow-[0_4px_15px_rgba(0,0,0,0.5)]">
-                <iframe 
+                <iframe
                   className="absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/p4t7UKNjGm8?si=nTmngCQr8fIeOt_V" 
+                  src="https://www.youtube.com/embed/p4t7UKNjGm8?si=nTmngCQr8fIeOt_V"
+                  title="How to Verify Your Account"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </section>
+            <section className="tutorial-section">
+              <h2 className="font-serif text-2xl md:text-3xl mb-6 text-white">
+                3. How to Deposit & Withdraw Funds
+              </h2>
+              <div className="relative pb-[56.25%] h-0 overflow-hidden rounded-lg border border-accent-color/20 shadow-[0_4px_15px_rgba(0,0,0,0.5)]">
+                <iframe
+                  className="absolute top-0 left-0 w-full h-full"
+                  src="https://www.youtube.com/embed/sMKv9czbF30?si=v4WkOww6qLZVMKMK"
                   title="How to Verify Your Account"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
